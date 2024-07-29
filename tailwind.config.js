b49e66dd6e3b5d3fba6b535192ddddcc5919b1ff
zaidihusain <zaidihusain611@gmai.com>
2024-07-29 21:44:01 +0530

    Add node_modules to .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ee0181',
+      },
+      fontFamily: {
+        'Husain-font':['Poppins , sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
